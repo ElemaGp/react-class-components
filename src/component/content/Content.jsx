3be85eb6df30bfc98e7c71b2394b875d9edb5fc8
ElemaGp <elemagodspower@gmail.com>
2023-01-17 22:06:59 +0100

    feat: created state and changed the state

--- src/component/content/Content.jsx
+++ src/component/content/Content.jsx
@@ -2,9 +2,33 @@ import React from "react";
 
 export default class Content extends React.Component {
 
+    state={
+        name: "IK",
+        id: 200
+    }
+
+    changeName = () => {
+        this.setState({
+            name: "James",
+            id: 5
+          })  
+        }
+
+        
+    
+
     render(){
+        console.log(this.state)
+
         return(
-            <div>{this.props.children}</div>
+            <div>
+                {this.props.children}
+              <div>
+                {this.state.name}
+              </div>
+              <button onClick={this.changeName}>Change state</button>
+            </div>
         )
     }
 }
+
