c794d67bc8991cb4cec3ded20af5b8724e330cc2
ElemaGp <elemagodspower@gmail.com>
2023-01-18 23:21:54 +0100

    feat: dynamically displayed the link based on input

--- src/component/classUsingConstructor/ClassUsingConstructor.jsx
+++ src/component/classUsingConstructor/ClassUsingConstructor.jsx
@@ -5,16 +5,18 @@ export default class ClassUsingConstructor extends React.Component {
         super(props)
         this.state = {
             name: "Vic",
-            age: 17
+            age: 17,
+            link: ""
         }
 
         this.changeName = this.changeName.bind(this)
     }
 
-    changeName() {
+    changeName(e) {
         this.setState({
             name: "Collins",
-            age: 28
+            age: 28,
+            link: e.target.value
         })
     }
 
@@ -26,6 +28,8 @@ export default class ClassUsingConstructor extends React.Component {
                 <p>{this.state.name} has a {this.props.car}</p>
                 <p>{this.state.age}</p>
                 <button onClick={this.changeName}>Change Name</button>
+                <input type="textarea" value={this.state.link} onChange={this.changeName}/>
+                <a href={`https://${this.state.link}`}>{this.state.link}</a>
             </div>
         )
     }
